e8da92f57248d0569dc14885ba028f28bda20203
Nguyễn Anh Tuấn <tuan.nguyen2208nat@hcmut.edu.vn>
2024-01-16 22:48:20 +0700

    git add

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 import sys
 from Adafruit_IO import MQTTClient
+import time
+import random
 
-AIO_FEED_ID = ""
-AIO_USERNAME = ""
-AIO_KEY = ""
+AIO_FEED_IDs = ["nutnhan1", "nutnhan2"]
+AIO_USERNAME = "tuannguyen2208nat"
+AIO_KEY = "aio_kmaj125CriS8gJJNevQ1gc1Xt9pb"
 
 def connected(client):
     print("Ket noi thanh cong ...")
-    client.subscribe(AIO_FEED_ID)
+    for topic in AIO_FEED_IDs:
+        client.subscribe(topic)
 
-def subscribe(client , userdata , mid , granted_qos):
+def subscribe(client, userdata, mid, granted_qos):
     print("Subscribe thanh cong ...")
 
 def disconnected(client):
     print("Ngat ket noi ...")
-    sys.exit (1)
+    sys.exit(1)
 
-def message(client , feed_id , payload):
+def message(client, feed_id, payload):
     print("Nhan du lieu: " + payload)
 
-client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client = MQTTClient(AIO_USERNAME, AIO_KEY)
 client.on_connect = connected
 client.on_disconnect = disconnected
 client.on_message = message
 client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
-
+counter=10
 while True:
+    counter=counter-1
+    if(counter<=0):
+        counter=10
+        # TO DO
+        print("Random data is publising...")
+        temp=random.randint(10,20)
+        client.publish("cambien1",temp)
+        humi =random.randint(50,70)
+        client.publish("cambien2",humi)
+        light=random.randint(100,500)
+        client.publish("cambien3",light)
+   
+    time.sleep(1)
     pass
